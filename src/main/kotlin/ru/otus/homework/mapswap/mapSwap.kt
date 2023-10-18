ab2dc66805c33e032c5be7dbf8c2678b2a9c0366
Ponyu-dev <mopoypa@yandex.ru>
2023-10-18 09:29:56 +0300

    Completed task 3. Map swap.

--- src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
+++ src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
@@ -3,4 +3,13 @@ package ru.otus.homework.mapswap
 /**
  * Меняет местами ключи и значения
  */
-fun <K, V> Map<K, V>.swap(): Map<V, K> = TODO("Доделать swap")
+fun <K, V> Map<K, V>.swap(): Map<V, K> = this.entries.associate { (key, value) -> value to key }
+fun <K, V> Map<K, V>.swapForEach(): Map<V, K> {
+    val swappedMap = mutableMapOf<V, K>()
+    this.forEach { (key, value) ->
+        swappedMap[value] = key
+    }
+    return swappedMap
+}
+
+fun <K, V> Map<K, V>.swapMapValues(): Map<V, K> = this.mapValues { it.value to it.key }.map { it.value }.toMap()

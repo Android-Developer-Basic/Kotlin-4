8ec7adefd068cb4abb3442938f03aeb11d9951c8
Artem Nagorny <agnagor3@mts.ru>
2023-10-19 12:37:50 +0300

    completed function sortByName

--- src/main/kotlin/ru/otus/homework/persons/persons.kt
+++ src/main/kotlin/ru/otus/homework/persons/persons.kt
@@ -10,4 +10,16 @@ fun List<Person>.sortByAge(): List<Person> = sortedByDescending { it.age }
  * - Фамилии сортируются по алфавиту в порядке возрастания
  * - Если фамилии совпадают, персоны сортируются по имени в порядке возрастания
  */
-fun List<Person>.sortByName(): List<Person> = TODO("Доделать sortBySurname")
+fun List<Person>.sortByName(): List<Person> = sortedWith { p0, p1 ->
+  if (p0.surname == p1.surname) {
+    if (p0.name > p1.name) {
+      1
+    } else if (p0.name < p1.name) {
+      -1
+    } else 0
+  } else {
+    if (p0.surname > p1.surname) {
+      1
+    } else -1
+  }
+}

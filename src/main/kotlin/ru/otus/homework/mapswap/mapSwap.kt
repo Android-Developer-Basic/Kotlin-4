b5b76221b746b07b5d072957892a6dcbbbc85a3f
Tuged <104727740+Tuged@users.noreply.github.com>
2023-10-20 04:21:21 +0800

    Update mapSwap.kt
    Меняет местами ключи и значения

--- src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
+++ src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
@@ -3,4 +3,10 @@ package ru.otus.homework.mapswap
 /**
  * Меняет местами ключи и значения
  */
-fun <K, V> Map<K, V>.swap(): Map<V, K> = TODO("Доделать swap")
+fun <K, V> Map<K, V>.swap(): Map<V, K> {
+    var result= mutableMapOf<V,K>()
+    for(entry in this){
+        result.put(entry.value, entry.key)
+    }
+    return result
+}

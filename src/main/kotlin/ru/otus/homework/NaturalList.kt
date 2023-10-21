8ff9771ca4ea535d28cbd17ecc3be9e8df0bbf16
SR <serod1306@gmail.com>
2023-10-22 02:00:27 +0300

    Выполненые домашние задания.

--- src/main/kotlin/ru/otus/homework/NaturalList.kt
+++ src/main/kotlin/ru/otus/homework/NaturalList.kt
@@ -1,5 +1,7 @@
 package ru.otus.homework
 
+import java.util.Arrays
+
 /**
  * Список натуральных чисел от 1 до n
  * @param n Последнее натуральное число в списке
@@ -35,14 +37,14 @@ class NaturalList(n: Int) : List<Int> {
      * Вернуть под-список этого списка, включая [fromIndex] и НЕ включая [toIndex]
      */
     override fun subList(fromIndex: Int, toIndex: Int): List<Int> {
-        TODO("Not yet implemented")
+        return this.filterIndexed { index, _ -> (fromIndex<=index) && (index<toIndex) }
     }
 
     /**
      * Returns true if list contains all numbers in the collection
      */
     override fun containsAll(elements: Collection<Int>): Boolean {
-        TODO("Not yet implemented")
+        return (elements-this).isEmpty()
     }
 
     override fun toString(): String {
@@ -53,13 +55,23 @@ class NaturalList(n: Int) : List<Int> {
      * Функция должна возвращать true, если сравнивается с другой реализацией списка тех же чисел
      * Например, NaturalList(5) должен быть равен listOf(1,2,3,4,5)
      */
-    override fun equals(other: Any?): Boolean = false
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other is List<*>) {
+            if (size != other.size) return false
+            return true
+        }
+        return false
+    }
 
     /**
      * Функция должна возвращать тот же hash-code, что и список другой реализации тех же чисел
      * Например, NaturalList(5).hashCode() должен быть равен listOf(1,2,3,4,5).hashCode()
      */
-    override fun hashCode(): Int = -1
+    override fun hashCode(): Int {
+        // реализаци как у массивов
+        return Arrays.hashCode(this.toIntArray())
+    }
 }
 
 private class NaturalIterator(private val n: Int) : Iterator<Int> {

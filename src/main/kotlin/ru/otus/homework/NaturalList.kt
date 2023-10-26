414eff1b90e4bea1738e5e8d35a705fe2a32dba2
Pak <helicopterpak@gmail.com>
2023-10-26 17:50:55 +0300

    task 5

--- src/main/kotlin/ru/otus/homework/NaturalList.kt
+++ src/main/kotlin/ru/otus/homework/NaturalList.kt
@@ -61,13 +61,27 @@ class NaturalList(n: Int) : List<Int> {
      * Функция должна возвращать true, если сравнивается с другой реализацией списка тех же чисел
      * Например, NaturalList(5) должен быть равен listOf(1,2,3,4,5)
      */
-    override fun equals(other: Any?): Boolean = false
+    override fun equals(other: Any?): Boolean {
+        if (other === null || other !is List<*>) return false
+        if (this.size != other.size) return false
+        for (i in this.indices) {
+            if (this[i] != other[i]) return false
+        }
+        return true
+
+    }
 
     /**
      * Функция должна возвращать тот же hash-code, что и список другой реализации тех же чисел
      * Например, NaturalList(5).hashCode() должен быть равен listOf(1,2,3,4,5).hashCode()
      */
-    override fun hashCode(): Int = -1
+    override fun hashCode(): Int {
+        var result = 1
+        for (element in this) {
+            result = 31 * result + element.hashCode()
+        }
+        return result
+    }
 }
 
 private class NaturalIterator(private val n: Int) : Iterator<Int> {
@@ -81,7 +95,7 @@ private class NaturalIterator(private val n: Int) : Iterator<Int> {
 }
 
 private class NaturalListIterator(private val n: Int, index: Int = 0) : ListIterator<Int> {
-    private var index:Int = index.coerceIn(0, n - 1)
+    private var index: Int = index.coerceIn(0, n - 1)
     override fun hasNext(): Boolean = index < n
     override fun hasPrevious(): Boolean = index > 0
     override fun next(): Int = if (hasNext()) {
@@ -89,11 +103,13 @@ private class NaturalListIterator(private val n: Int, index: Int = 0) : ListIter
     } else {
         throw NoSuchElementException()
     }
+
     override fun nextIndex(): Int = index
     override fun previous(): Int = if (hasPrevious()) {
         index--
     } else {
         throw NoSuchElementException()
     }
+
     override fun previousIndex(): Int = index
 }

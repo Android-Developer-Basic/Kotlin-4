0e401a33ba2c1d803765f7bc13fdeaa1fdac8546
Alexey Kozyrev <tcsid4@gmail.com>
2023-10-28 08:45:47 +0300

    Готовая домашняя  работа

--- src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
+++ src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
@@ -3,4 +3,11 @@ package ru.otus.homework.mapswap
 /**
  * Меняет местами ключи и значения
  */
-fun <K, V> Map<K, V>.swap(): Map<V, K> = TODO("Доделать swap")
+fun <K, V> Map<K, V>.swap(): Map<V, K> {
+
+    var newMap = mutableMapOf<V,K>()
+    for ((key, value ) in this){
+        newMap[value] = key
+    }
+    return newMap
+}

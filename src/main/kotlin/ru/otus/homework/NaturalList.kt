50d425477e6c3f5fca8edb8fd7ae8576ff1c202d
dmilkov97 <denismilkov1@yandex.ru>
2023-12-04 21:20:52 +0700

    Реализация 4 пункта (subList, containsAll)

--- src/main/kotlin/ru/otus/homework/NaturalList.kt
+++ src/main/kotlin/ru/otus/homework/NaturalList.kt
@@ -35,14 +35,26 @@ class NaturalList(n: Int) : List<Int> {
      * Вернуть под-список этого списка, включая [fromIndex] и НЕ включая [toIndex]
      */
     override fun subList(fromIndex: Int, toIndex: Int): List<Int> {
-        TODO("Not yet implemented")
+        val list: MutableList<Int> = mutableListOf()
+        for(it in fromIndex until toIndex) list.add(this[it])
+        return list
     }
 
     /**
      * Returns true if list contains all numbers in the collection
      */
     override fun containsAll(elements: Collection<Int>): Boolean {
-        TODO("Not yet implemented")
+        for(it in this) {
+            var isContain:Boolean = false
+            for(it2 in elements) {
+                if (it == it2) {
+                    isContain = true
+                    break
+                }
+            }
+            if (!isContain) return false
+        }
+        return true
     }
 
     override fun toString(): String {

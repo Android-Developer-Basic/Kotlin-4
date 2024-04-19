cc30d4429fe4f21373e7b814deecda4c14ad4dd4
Nikolay (nazarovNV) <nikolaynazarovvv216@gmail.com>
2024-04-19 19:56:07 +0300

    Сделал третье

--- src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
+++ src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
@@ -1,6 +1,14 @@
 package ru.otus.homework.mapswap
 
+import org.junit.jupiter.api.Assertions
+
 /**
  * Меняет местами ключи и значения
  */
-fun <K, V> Map<K, V>.swap(): Map<V, K> = TODO("Доделать swap")
+fun <K, V> Map<K, V>.swap(): Map<V, K> {
+    val result = mutableMapOf<V, K>()
+    for ((key, value) in this) {
+        result[value] = key
+    }
+    return result
+}

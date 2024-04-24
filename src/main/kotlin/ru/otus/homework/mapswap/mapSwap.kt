e431c9ac7eec5bc009d14c50f3e4944b9f3c8aea
Gimasteev <best.roma7@gmail.com>
2024-04-24 13:11:14 +0500

    Переворот Map

--- src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
+++ src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
@@ -3,4 +3,10 @@ package ru.otus.homework.mapswap
 /**
  * Меняет местами ключи и значения
  */
-fun <K, V> Map<K, V>.swap(): Map<V, K> = TODO("Доделать swap")
+fun <K, V> Map<K, V>.swap(): Map<V, K>{
+    val newMap = HashMap<V, K>()
+    for ((key, value) in this) {
+        newMap[value] = key
+    }
+    return newMap
+}

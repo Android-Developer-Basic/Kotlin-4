948f1fc4cbcf048d7dc2c4210c3107033e4ab52a
Gimasteev <best.roma7@gmail.com>
2024-04-27 13:28:58 +0500

    Переворот Map - убрал HashMap и попробовал без него. Все работает

--- src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
+++ src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
@@ -4,8 +4,8 @@ package ru.otus.homework.mapswap
  * Меняет местами ключи и значения
  */
 fun <K, V> Map<K, V>.swap(): Map<V, K>{
-    val newMap = HashMap<V, K>()
-    for ((key, value) in this) {
+    val newMap = mutableMapOf<V, K>()
+    for ((key, value ) in this){
         newMap[value] = key
     }
     return newMap

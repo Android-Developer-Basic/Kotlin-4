a31f501fef3a1f7a586c0a71a26a5334f2c29d43
Evgenii Balandin <e.balandin@dt1520.ru>
2024-10-18 19:52:52 +0300

    Выполнил домашнее задание Kotlin-4

--- src/main/kotlin/ru/otus/homework/NaturalList.kt
+++ src/main/kotlin/ru/otus/homework/NaturalList.kt
@@ -35,14 +35,16 @@ class NaturalList(n: Int) : List<Int> {
      * Вернуть под-список этого списка, включая [fromIndex] и НЕ включая [toIndex]
      */
     override fun subList(fromIndex: Int, toIndex: Int): List<Int> {
-        TODO("Not yet implemented")
+        if (fromIndex !in 0..size || toIndex !in 0..size || fromIndex > toIndex)
+            throw IndexOutOfBoundsException("Invalid indices: fromIndex=$fromIndex, toIndex=$toIndex, Size=$size")
+        return (fromIndex + 1 until toIndex + 1).toList()
     }
 
     /**
      * Returns true if list contains all numbers in the collection
      */
     override fun containsAll(elements: Collection<Int>): Boolean {
-        TODO("Not yet implemented")
+        return elements.all { it in 1..size }
     }
 
     override fun toString(): String {
@@ -53,13 +55,19 @@ class NaturalList(n: Int) : List<Int> {
      * Функция должна возвращать true, если сравнивается с другой реализацией списка тех же чисел
      * Например, NaturalList(5) должен быть равен listOf(1,2,3,4,5)
      */
-    override fun equals(other: Any?): Boolean = false
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is List<*>) return false
+        return (1..size).toList() == other
+    }
 
     /**
      * Функция должна возвращать тот же hash-code, что и список другой реализации тех же чисел
      * Например, NaturalList(5).hashCode() должен быть равен listOf(1,2,3,4,5).hashCode()
      */
-    override fun hashCode(): Int = -1
+    override fun hashCode(): Int {
+        return (1..size).toList().hashCode()
+    }
 }
 
 private class NaturalIterator(private val n: Int) : Iterator<Int> {

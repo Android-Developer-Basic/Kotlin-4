7f7dbee92e39e272416af8c53bfa5536ed31c600
SpiritualAdviser <pomaxam@narod.ru>
2024-10-19 11:21:24 +0300

    add fun sortByNam - the persons were sorted alphabetically

--- src/main/kotlin/ru/otus/homework/persons/persons.kt
+++ src/main/kotlin/ru/otus/homework/persons/persons.kt
@@ -1,3 +1,8 @@
 package ru.otus.homework.persons
 
 fun List<Person>.sortByAge(): List<Person> = this.sortedByDescending { it.age }
+
+fun List<Person>.sortByName(): List<Person> =
+    this.sortedWith(compareBy<Person> { it.surname }.thenBy { it.name })
+
+

59d4d455ad2629d1d954994af488e970dc9072d9
GIDROPES <gidropes2002@gmail.com>
2024-11-05 20:51:08 +0300

    Task 3 completed. Tests passed

--- src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
+++ src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
@@ -3,4 +3,8 @@ package ru.otus.homework.mapswap
 /**
  * Меняет местами ключи и значения
  */
-fun <K, V> Map<K, V>.swap(): Map<V, K> = TODO("Доделать swap")
+fun <K, V> Map<K, V>.swap(): Map<V, K>{
+    val resultMap:MutableMap<V,K> = mutableMapOf()
+    forEach{resultMap.put(it.value,it.key)}
+    return resultMap.toMap()
+}

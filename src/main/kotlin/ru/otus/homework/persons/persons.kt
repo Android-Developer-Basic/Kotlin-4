76aed06094e9185e01de0d7fede545f07b40c2de
GIDROPES <gidropes2002@gmail.com>
2024-11-05 20:33:08 +0300

    Task 2 completed with CustomComparator. Tests passed

--- src/main/kotlin/ru/otus/homework/persons/persons.kt
+++ src/main/kotlin/ru/otus/homework/persons/persons.kt
@@ -1,5 +1,7 @@
 package ru.otus.homework.persons
 
+import java.util.Comparator
+
 /**
  * Отсортировать список персон по возрасту в порядке убывания
  *
@@ -13,7 +15,17 @@ fun List<Person>.sortByAge(): List<Person> = sortedByDescending { it.age }
  * - Если фамилии совпадают, персоны сортируются по имени в порядке возрастания
  */
 fun List<Person>.sortByName(): List<Person>{
-    return sortedWith(
-        compareBy{ it.name }
-    )
+    val customComparator = Comparator{ obj1: Person, obj2: Person ->
+            if(obj1.surname != obj2.surname){
+                if (obj1.surname > obj2.surname) return@Comparator 1
+                else return@Comparator -1
+            }
+            else{
+                if (obj1.name > obj2.name) return@Comparator 1
+                else if (obj1.name < obj2.name) return@Comparator -1
+                else return@Comparator 0
+            }
+         }
+
+    return sortedWith(customComparator)
 }

b747c9123ad42a4ba822f8694bb845ea97a9f684
GrAlexandrov <alexandrov.18rus@gmail.com>
2024-11-06 17:56:00 +0500

    Домашнее задание 3

--- src/main/kotlin/ru/otus/homework/NaturalList.kt
+++ src/main/kotlin/ru/otus/homework/NaturalList.kt
@@ -35,14 +35,14 @@ class NaturalList(n: Int) : List<Int> {
      * Вернуть под-список этого списка, включая [fromIndex] и НЕ включая [toIndex]
      */
     override fun subList(fromIndex: Int, toIndex: Int): List<Int> {
-        TODO("Not yet implemented")
+        return (fromIndex..(toIndex)).toList()-1
     }
 
     /**
      * Returns true if list contains all numbers in the collection
      */
     override fun containsAll(elements: Collection<Int>): Boolean {
-        TODO("Not yet implemented")
+        return elements.all {it in NaturalList(size)  }
     }
 
     override fun toString(): String {
@@ -53,13 +53,29 @@ class NaturalList(n: Int) : List<Int> {
      * Функция должна возвращать true, если сравнивается с другой реализацией списка тех же чисел
      * Например, NaturalList(5) должен быть равен listOf(1,2,3,4,5)
      */
-    override fun equals(other: Any?): Boolean = false
-
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if ((other !is List<*>)||( other.count()!= this.count() )) return false
+        var booleanVariable = true
+        for (i: Int in this.indices) {
+            if (this[i] != other[i] ){
+                booleanVariable = false
+           }
+       }
+       return booleanVariable
+    }
     /**
      * Функция должна возвращать тот же hash-code, что и список другой реализации тех же чисел
      * Например, NaturalList(5).hashCode() должен быть равен listOf(1,2,3,4,5).hashCode()
      */
-    override fun hashCode(): Int = -1
+    override fun hashCode(): Int {
+        var hashCode = 1
+        for (i in this) {
+            hashCode = 31 * hashCode + (i.hashCode())
+        }
+        return hashCode
+    }
+
 }
 
 private class NaturalIterator(private val n: Int) : Iterator<Int> {
--- src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
+++ src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
@@ -3,4 +3,6 @@ package ru.otus.homework.mapswap
 /**
  * Меняет местами ключи и значения
  */
-fun <K, V> Map<K, V>.swap(): Map<V, K> = TODO("Доделать swap")
+fun <K, V> Map<K, V>.swap(): Map<V, K>{
+    return map { it -> Pair(it.value, it.key) }.toMap()
+}

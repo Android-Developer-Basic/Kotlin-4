d50d3bd8314aa6f53011a83f51128633fa60b7f3
AT <azriel.torgovetskiy@gmail.com>
2024-12-02 22:54:50 +0300

    Implemented hw

--- src/main/kotlin/ru/otus/homework/NaturalList.kt
+++ src/main/kotlin/ru/otus/homework/NaturalList.kt
@@ -35,14 +35,29 @@ class NaturalList(n: Int) : List<Int> {
      * Вернуть под-список этого списка, включая [fromIndex] и НЕ включая [toIndex]
      */
     override fun subList(fromIndex: Int, toIndex: Int): List<Int> {
-        TODO("Not yet implemented")
+        checkRange(fromIndex, toIndex)
+        val subListIterator = SubListNaturalIterator(fromIndex, toIndex)
+        val subList = mutableListOf<Int>()
+        for (i in subListIterator) {
+            subList.add(i)
+        }
+        return subList
+    }
+
+    private fun checkRange(fromIndex: Int, toIndex: Int) {
+        if (fromIndex < 0) throw IndexOutOfBoundsException("fromIndex less than 0: $fromIndex")
+        if (toIndex > size) throw IndexOutOfBoundsException("toIndex more than size: $toIndex")
+        if (fromIndex >= toIndex) throw IllegalArgumentException("fromIndex more than toIndex: fromIndex: $fromIndex toIndex: $toIndex")
     }
 
     /**
      * Returns true if list contains all numbers in the collection
      */
     override fun containsAll(elements: Collection<Int>): Boolean {
-        TODO("Not yet implemented")
+        for (e in elements)
+            if (!contains(e))
+                return false
+        return true
     }
 
     override fun toString(): String {
@@ -53,13 +68,27 @@ class NaturalList(n: Int) : List<Int> {
      * Функция должна возвращать true, если сравнивается с другой реализацией списка тех же чисел
      * Например, NaturalList(5) должен быть равен listOf(1,2,3,4,5)
      */
-    override fun equals(other: Any?): Boolean = false
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is List<*>) return false
+        if (size != other.size) return false
+        for (i in 0 until size) {
+            if (other[i] != this[i]) return false
+        }
+        return true
+    }
 
     /**
      * Функция должна возвращать тот же hash-code, что и список другой реализации тех же чисел
      * Например, NaturalList(5).hashCode() должен быть равен listOf(1,2,3,4,5).hashCode()
      */
-    override fun hashCode(): Int = -1
+    override fun hashCode(): Int {
+        var hashCode = 1
+        for (i in this) {
+            hashCode = 31 * hashCode + i
+        }
+        return hashCode
+    }
 }
 
 private class NaturalIterator(private val n: Int) : Iterator<Int> {
@@ -72,8 +101,18 @@ private class NaturalIterator(private val n: Int) : Iterator<Int> {
     }
 }
 
+private class SubListNaturalIterator(fromIndex: Int, private val toIndex: Int) : Iterator<Int> {
+    private var index = fromIndex
+    override fun hasNext(): Boolean = index < toIndex
+    override fun next(): Int = if (hasNext()) {
+        ++index
+    } else {
+        throw NoSuchElementException()
+    }
+}
+
 private class NaturalListIterator(private val n: Int, index: Int = 0) : ListIterator<Int> {
-    private var index:Int = index.coerceIn(0, n - 1)
+    private var index: Int = index.coerceIn(0, n - 1)
     override fun hasNext(): Boolean = index < n
     override fun hasPrevious(): Boolean = index > 0
     override fun next(): Int = if (hasNext()) {
@@ -81,11 +120,13 @@ private class NaturalListIterator(private val n: Int, index: Int = 0) : ListIter
     } else {
         throw NoSuchElementException()
     }
+
     override fun nextIndex(): Int = index
     override fun previous(): Int = if (hasPrevious()) {
         index--
     } else {
         throw NoSuchElementException()
     }
+
     override fun previousIndex(): Int = index
 }

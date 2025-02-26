b065d1718fa34c0cc13bab058f81e2b840c4f822
Anv0l <andrei.voloshin@live.com>
2025-02-26 23:20:23 +0300

    ДЗ № 3
    1. Сортировка по возрасту по убыванию (persons.kt): сделал -it.age, можно было sortedByDescending. Как лучше/правильнее?
    
    2. Сортировка по фамилии и имени (persons.kt).
    
    3. Переворот map (mapSwap.kt). Не разобрался сам, как сделать через функции Котлина. Нашёл в интернете пару реализаций (например, через this.entries.associate, или через map{}), оставил в комментариях.
    
    4. Реализация subList и containsAll (NaturalList.kt).
    
    5. Реализация equals() и hashCode(). Не уверен в корректности реализации обеих функций. hashCode() и вовсе скопироал из реализации AbstractList.
    
    Зато все тесты проходит. :)

--- src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
+++ src/main/kotlin/ru/otus/homework/mapswap/mapSwap.kt
@@ -3,4 +3,14 @@ package ru.otus.homework.mapswap
 /**
  * Меняет местами ключи и значения
  */
-fun <K, V> Map<K, V>.swap(): Map<V, K> = TODO("Доделать swap")
+fun <K, V> Map<K, V>.swap(): Map<V, K> {
+//    return this.map { (key, value) -> value to key }.toMap()
+//    return this.entries.associate { (key, value) -> value to key }
+    val result: MutableMap<V, K> = emptyMap<V, K>().toMutableMap()
+
+    for (m in this) {
+        result[m.value] = m.key
+    }
+
+    return result.toMap()
+}

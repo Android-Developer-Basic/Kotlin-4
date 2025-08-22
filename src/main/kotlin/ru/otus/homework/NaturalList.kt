19392186b0e10c84739d4528ca51485e9194d8eb
Vp-Ma <vagrant18pm@gmail.com>
2025-08-22 13:53:36 +0300

    Homework_3 implementation(NaturalList.kt)

--- src/main/kotlin/ru/otus/homework/NaturalList.kt
+++ src/main/kotlin/ru/otus/homework/NaturalList.kt
@@ -35,14 +35,18 @@ class NaturalList(n: Int) : List<Int> {
      * Вернуть под-список этого списка, включая [fromIndex] и НЕ включая [toIndex]
      */
     override fun subList(fromIndex: Int, toIndex: Int): List<Int> {
-        TODO("Not yet implemented")
+
+        return this.toList().slice(fromIndex..toIndex - 1)
+
     }
 
     /**
      * Returns true if list contains all numbers in the collection
      */
     override fun containsAll(elements: Collection<Int>): Boolean {
-        TODO("Not yet implemented")
+
+        return this.toList().containsAll(elements)
+
     }
 
     override fun toString(): String {
@@ -53,13 +57,30 @@ class NaturalList(n: Int) : List<Int> {
      * Функция должна возвращать true, если сравнивается с другой реализацией списка тех же чисел
      * Например, NaturalList(5) должен быть равен listOf(1,2,3,4,5)
      */
-    override fun equals(other: Any?): Boolean = false
+    override fun equals(other: Any?): Boolean {
+
+        var result = this === other
+        if (result) {
+            return result
+        } else if (other != null && other is List<*> && this.size == other.size) {
+            for (i in this.indices) {
+                if (this[i] != other[i]) return result
+            }
+            result = true
+        }
+        return result
+    }
 
     /**
      * Функция должна возвращать тот же hash-code, что и список другой реализации тех же чисел
      * Например, NaturalList(5).hashCode() должен быть равен listOf(1,2,3,4,5).hashCode()
      */
-    override fun hashCode(): Int = -1
+    override fun hashCode(): Int {
+       var hashCode = 1
+       for( item in this )
+           hashCode = 31 * hashCode + item
+       return hashCode
+    }
 }
 
 private class NaturalIterator(private val n: Int) : Iterator<Int> {
